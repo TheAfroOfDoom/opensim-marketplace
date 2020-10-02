a1578d5bb75d121fae7ae16bf03b8aa5c7c09346
rwickett51 <ryan@wickettonline.com>
2020-10-02 15:23:04 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
