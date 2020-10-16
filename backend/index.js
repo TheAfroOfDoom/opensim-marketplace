82d44e7900b5da2954aa7118da9cbd12c5fa5fee
rwickett51 <ryan@wickettonline.com>
2020-10-16 13:20:30 -0400

    Fixed backend crashing on repeat requests

--- backend/index.js
+++ backend/index.js
@@ -23,54 +23,45 @@ const marketplace = mysql.createConnection({
   database: "opensim_marketplace",
 });
 
+marketplace.connect();
+opensim.connect();
+
 /* Endpoints */
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  return res.send("Hello World!");
 });
 
 app.get("/test", (req, res) => {
-  //Attempt Database Connection
-  marketplace.connect((err) => {
-    if (err) res.send("Not Connected");
-
-    marketplace.query("SELECT * FROM test_table", (err, result, fields) => {
-      console.log(fields);
-      res.send({ fields, result });
-    });
+  marketplace.query("SELECT * FROM test_table", (err, result, fields) => {
+    console.log(fields);
+    return res.send({ fields, result });
   });
 });
 
 app.get("/login", (req, res) => {
   //Destructure Username and Password params
   const { firstName, lastName, password } = req.query;
-  console.log(firstName, lastName, password);
-
-  //Attempt Database Connection
-  opensim.connect((err) => {
-    if (err) return res.send("Not Connected");
-
-    // Attempt SQL Query
-    opensim.query(
-      `SELECT auth.UUID, auth.passwordHash, auth.passwordSalt FROM auth INNER JOIN useraccounts ON useraccounts.PrincipalID=auth.UUID WHERE LastName="${lastName}" and FirstName="${firstName}";`,
-      (err, result, fields) => {
-        //Check to see if password matches
-        console.log(result[0]);
-        let hashedPassword = md5(md5(password) + ":" + result[0].passwordSalt);
-        if (hashedPassword === result[0].passwordHash)
-          return res.send("User Authenticated");
-        return res.send("Name/Password Does not match");
+  // Attempt SQL Query
+  opensim.query(
+    `SELECT auth.UUID, auth.passwordHash, auth.passwordSalt FROM auth INNER JOIN useraccounts ON useraccounts.PrincipalID=auth.UUID WHERE LastName="${lastName}" and FirstName="${firstName}";`,
+    (err, result, fields) => {
+      //Check to see if password matches
+      user = result[0];
+      console.log(user);
+      let hashedPassword = md5(md5(password) + ":" + user.passwordSalt);
+      if (hashedPassword === user.passwordHash) {
+        return res.send("User Authenticated");
       }
-    );
-  });
+      return res.send("Name/Password Does not match");
+    }
+  );
 });
 
 app.get("/item", (req, res) => {
-  /*
   marketplace.query("SELECT * from auth", (err, result, fields) => {
-    res.send(result);
+    return res.send(result);
   });
-  */
 });
 
 app.listen(port, () => {
