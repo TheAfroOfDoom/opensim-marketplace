aadf8138eac328d5a6d611731218b60c7b65d93c
rwickett51 <ryan@wickettonline.com>
2020-10-16 13:48:35 -0400

    Auth returns webLoginKey. now checks all pairs

--- backend/index.js
+++ backend/index.js
@@ -44,15 +44,19 @@ app.get("/login", (req, res) => {
   const { firstName, lastName, password } = req.query;
   // Attempt SQL Query
   opensim.query(
-    `SELECT auth.UUID, auth.passwordHash, auth.passwordSalt FROM auth INNER JOIN useraccounts ON useraccounts.PrincipalID=auth.UUID WHERE LastName="${lastName}" and FirstName="${firstName}";`,
+    `SELECT auth.UUID, auth.passwordHash, auth.passwordSalt, auth.webLoginKey FROM auth INNER JOIN useraccounts ON useraccounts.PrincipalID=auth.UUID WHERE LastName="${lastName}" and FirstName="${firstName}";`,
     (err, result, fields) => {
       //Check to see if password matches
-      user = result[0];
-      console.log(user);
-      let hashedPassword = md5(md5(password) + ":" + user.passwordSalt);
-      if (hashedPassword === user.passwordHash) {
-        return res.send("User Authenticated");
+      for (let i = 0; i < result.length; i++) {
+        user = result[i];
+        console.log(user);
+        let hashedPassword = md5(md5(password) + ":" + user.passwordSalt);
+        if (hashedPassword === user.passwordHash) {
+          let obj;
+          return res.send({ UUID: user.UUID, webLoginKey: user.webLoginKey });
+        }
       }
+
       return res.send("Name/Password Does not match");
     }
   );
