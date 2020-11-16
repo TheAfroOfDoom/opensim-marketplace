97875d02dc4e90d972072ae83d31d1c06e708f43
rwickett51 <ryan@wickettonline.com>
2020-11-16 11:43:52 -0500

    Refined search query code

--- backend/src/api/routes/search.js
+++ backend/src/api/routes/search.js
@@ -12,108 +12,27 @@ router.get("/", async (req, res) => {
     const { uuid } = req.cookies;
     if (uuid === undefined) throw new Error();
 
+    // Give relations
+    UserAccounts.hasMany(Assets);
+    Assets.belongsTo(UserAccounts);
+
     // Check if there is a valid search string
     let { searchString, assetType } = req.query;
     if (searchString === undefined) {
       searchString = "";
     }
 
-    UserAccounts.hasMany(Assets);
-    Assets.belongsTo(UserAccounts);
+    //Assign query params
+    let whereParams = {
+      name: { [Op.like]: `%${searchString}%` },
+      [Op.or]: [{ public: 1 }, { builtin: 1 }],
+    };
 
-    let searchInfo;
-    if (assetType == undefined) {
-      // Search for all assets
-      searchInfo = await Assets.findAll({
-        where: {
-          name: { [Op.like]: `%${searchString}%` },
-          [Op.or]: [{ public: 1 }, { builtin: 1 }],
-        },
-        attributes: [
-          "name",
-          "description",
-          "assetType",
-          "id",
-          "create_time",
-          "CreatorID",
-          "access_time",
-        ],
-        include: [
-          {
-            model: UserAccounts,
-            attributes: ["FirstName", "LastName"],
-            required: true,
-            on: {
-              col1: sequelize.where(
-                sequelize.col("assets.CreatorID"),
-                "=",
-                sequelize.col("useraccount.PrincipalID")
-              ),
-            },
-          },
-        ],
-      });
-    } else {
-      // Search for all assets of type
-      searchInfo = await Assets.findAll({
-        where: {
-          name: { [Op.like]: `%${searchString}%` },
-          [Op.or]: [{ public: 1 }, { builtin: 1 }],
-          assetType: assetType,
-        },
-        attributes: [
-          "name",
-          "description",
-          "assetType",
-          "id",
-          "create_time",
-          "CreatorID",
-          "access_time",
-        ],
-        include: [
-          {
-            model: UserAccounts,
-            attributes: ["FirstName", "LastName"],
-            required: true,
-            on: {
-              col1: sequelize.where(
-                sequelize.col("assets.CreatorID"),
-                "=",
-                sequelize.col("useraccount.PrincipalID")
-              ),
-            },
-          },
-        ],
-      });
+    if (assetType !== undefined) {
+      whereParams.assetType = assetType;
     }
 
-    let invInfo = await Assets.findAll({
-      attributes: [
-        "name",
-        "description",
-        "assetType",
-        "id",
-        "create_time",
-        "CreatorID",
-        "access_time",
-      ],
-      include: [
-        {
-          model: UserAccounts,
-          attributes: ["FirstName", "LastName"],
-          required: true,
-          on: {
-            col1: sequelize.where(
-              sequelize.col("assets.CreatorID"),
-              "=",
-              sequelize.col("useraccount.PrincipalID")
-            ),
-          },
-        },
-      ],
-    });
-
-    return res.send(searchInfo);
+    return res.send(await getAssets(whereParams));
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
@@ -139,162 +58,49 @@ router.get("/public", async (req, res) => {
     //Assign query params
     let whereParams = {
       name: { [Op.like]: `%${searchString}%` },
-      [Op.or]: [{ public: 1 }, { builtin: 1 }],
+      [Op.or]: [{ public: 1 }],
     };
 
     if (assetType !== undefined) {
       whereParams.assetType = assetType;
     }
 
-    // Search for all assets
-    const searchInfo = await Assets.findAll({
-      where: {
-        ...whereParams,
+    return res.send(await getAssets(whereParams));
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
+
+function getAssets(whereParams) {
+  return Assets.findAll({
+    where: {
+      ...whereParams,
+    },
+    attributes: [
+      "name",
+      "description",
+      "assetType",
+      "id",
+      "create_time",
+      "CreatorID",
+      "access_time",
+    ],
+    include: [
+      {
+        model: UserAccounts,
+        attributes: ["FirstName", "LastName"],
+        required: true,
+        on: {
+          col1: sequelize.where(
+            sequelize.col("assets.CreatorID"),
+            "=",
+            sequelize.col("useraccount.PrincipalID")
+          ),
+        },
       },
-      attributes: [
-        "name",
-        "description",
-        "assetType",
-        "id",
-        "create_time",
-        "CreatorID",
-        "access_time",
-      ],
-      include: [
-        {
-          model: UserAccounts,
-          attributes: ["FirstName", "LastName"],
-          required: true,
-          on: {
-            col1: sequelize.where(
-              sequelize.col("assets.CreatorID"),
-              "=",
-              sequelize.col("useraccount.PrincipalID")
-            ),
-          },
-        },
-      ],
-    });
-
-    return res.send(searchInfo);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
-});
-
-router.get("/builtin", async (req, res) => {
-  try {
-    //Check if user is authenticated
-    const { uuid } = req.cookies;
-    if (uuid === undefined) throw new Error();
-
-    // Check if there is a valid search string
-    let { searchString, assetType } = req.query;
-    if (searchString === undefined) {
-      searchString = "";
-    }
-
-    UserAccounts.hasMany(Assets);
-    Assets.belongsTo(UserAccounts);
-
-    let searchInfo;
-    if (assetType == undefined) {
-      // Search for all assets
-      searchInfo = await Assets.findAll({
-        where: {
-          name: { [Op.like]: `%${searchString}%` },
-          builtin: 1,
-        },
-        attributes: [
-          "name",
-          "description",
-          "assetType",
-          "id",
-          "create_time",
-          "CreatorID",
-          "access_time",
-        ],
-        include: [
-          {
-            model: UserAccounts,
-            attributes: ["FirstName", "LastName"],
-            required: true,
-            on: {
-              col1: sequelize.where(
-                sequelize.col("assets.CreatorID"),
-                "=",
-                sequelize.col("useraccount.PrincipalID")
-              ),
-            },
-          },
-        ],
-      });
-    } else {
-      // Search for all assets of type
-      searchInfo = await Assets.findAll({
-        where: {
-          name: { [Op.like]: `%${searchString}%` },
-          [Op.or]: [{ public: 1 }, { builtin: 1 }],
-          assetType: assetType,
-        },
-        attributes: [
-          "name",
-          "description",
-          "assetType",
-          "id",
-          "create_time",
-          "CreatorID",
-          "access_time",
-        ],
-        include: [
-          {
-            model: UserAccounts,
-            attributes: ["FirstName", "LastName"],
-            required: true,
-            on: {
-              col1: sequelize.where(
-                sequelize.col("assets.CreatorID"),
-                "=",
-                sequelize.col("useraccount.PrincipalID")
-              ),
-            },
-          },
-        ],
-      });
-    }
-
-    let invInfo = await Assets.findAll({
-      attributes: [
-        "name",
-        "description",
-        "assetType",
-        "id",
-        "create_time",
-        "CreatorID",
-        "access_time",
-      ],
-      include: [
-        {
-          model: UserAccounts,
-          attributes: ["FirstName", "LastName"],
-          required: true,
-          on: {
-            col1: sequelize.where(
-              sequelize.col("assets.CreatorID"),
-              "=",
-              sequelize.col("useraccount.PrincipalID")
-            ),
-          },
-        },
-      ],
-    });
-
-    return res.send(searchInfo);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
-});
+    ],
+  });
+}
 
 module.exports = router;
