af3cb409d2c185e700d3c443457958d1f4cda0c0
rwickett51 <ryan@wickettonline.com>
2020-11-22 13:42:36 -0500

    Additional Fixes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
 const { logging } = require("../config/index");
 
-module.exports = new Sequelize("opensim", "ryanw", "2EvXhxnn", {
+module.exports = new Sequelize("opensim", "marketplace", "bLV8dkgQjjpq", {
   host: "25.1.197.128",
   dialect: "mysql",
+  logging: console.log(),
 });
